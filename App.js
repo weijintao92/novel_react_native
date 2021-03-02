2c140ae76831a2220ed74926687bbfe476bf7331
weijintao92 <47779463+weijintao92@users.noreply.github.com>
2021-03-02 18:06:48 +0800

    2
    2

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Try editing messs! 🎉</Text>
+    </View>
+  );
+};
+
+export default App;
